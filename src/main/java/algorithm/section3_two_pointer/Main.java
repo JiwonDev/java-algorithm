23f1c7e3e5a59392f7de46428cdb3b876ff5cc69
JiwonDev <sylvester7412@gmail.com>
2022-03-18 21:41:21 +0900

    Add Section4

--- src/main/java/algorithm/section3_two_pointer/Main.java
+++ src/main/java/algorithm/section3_two_pointer/Main.java
@@ -1,11 +1,5 @@
 package algorithm.section3_two_pointer;
 
-import java.util.Scanner;
-
 public final class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-    }
 }
